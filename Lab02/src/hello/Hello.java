5811b58628d499b04b7f8e58fca74083f56e5870
zinak <zinak@host.docker.internal>
2024-11-25 15:35:14 -0800

    uzair is so good

--- Lab02/src/hello/Hello.java
+++ Lab02/src/hello/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by Uzair Macy.");
+		System.out.println("SacStateID - This line was added by Zina Khan.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
