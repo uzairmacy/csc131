71a46ec249a403f34daa75e8673deb259f20876b
Uzair <Uzair@host.docker.internal>
2024-11-30 22:15:16 -0800

    macros list and output testing

--- CalorieCounter/src/calories/CalorieCounter.java
+++ CalorieCounter/src/calories/CalorieCounter.java
@@ -9,16 +9,19 @@ import utils.Utils;
 
 public class CalorieCounter {
 	public static void main(String args[]) {
+		Utils utils = new Utils();
+		
 		User newUser = new User(null, 0, 0, 0, null);
 		newUser.getUserInfo();
 		
-		System.out.println("Hey " + newUser.getFullName() + "! ");
+		System.out.println("Hey " + newUser.getFullName() + "! What did you eat today?");
 		
 		List<Macronutrients> macros = new ArrayList<>();
 		Macronutrients tmpMacros = new Macronutrients(null, 0, 0, 0, 0, 0);
 		tmpMacros.getMacros();
 		macros.add(tmpMacros);
 		
+		utils.Macromath(macros.get(0));
 		
 	}
 }
