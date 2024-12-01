f9b95f94bd723ec7d4328cbccd80e58a4d0e762e
Uzair <Uzair@host.docker.internal>
2024-11-30 22:00:02 -0800

    Merge branch 'master' of https://github.com/uzairmacy/csc131.git

--- CalorieCounter/src/calories/CalorieCounter.java
+++ CalorieCounter/src/calories/CalorieCounter.java
@@ -1,5 +1,8 @@
 package calories;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import user.User;
 import food.Macronutrients;
 import utils.Utils;
@@ -8,8 +11,13 @@ public class CalorieCounter {
 	public static void main(String args[]) {
 		User newUser = new User(null, 0, 0, 0, null);
 		newUser.getUserInfo();
-		Macronutrients macros = new Macronutrients(null, 0, 0, 0, 0, 0);
-		macros.getMacros();
+		
+		System.out.println("Hey " + newUser.get);
+		
+		List<Macronutrients> macros = new ArrayList<>();
+		Macronutrients tmpMacros = new Macronutrients(null, 0, 0, 0, 0, 0);
+		tmpMacros.getMacros();
+		macros.add(tmpMacros);
 		
 		
 	}
