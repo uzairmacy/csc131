a929cc46685103d5c5833d172c4deb380257967e
Uzair <Uzair@10.118.24.181>
2024-12-06 15:06:28 -0800

    improvements by zina i guess

--- CalorieCounter/src/calories/CalorieCounter.java
+++ CalorieCounter/src/calories/CalorieCounter.java
@@ -20,7 +20,7 @@ public class CalorieCounter {
 		newUser.getUserInfo();
 		
 		System.out.println("\n================================================================================================");
-		System.out.println("Thank you for the information, here is you bmi and how much more water you need to drink.");
+		System.out.println("    Thank you for the information, here is you bmi and how much more water you need to drink.");
 		System.out.println("================================================================================================");
 		
 		System.out.println("Your BMI: " + String.format("%.2f", newUser.BMI()));
@@ -30,7 +30,7 @@ public class CalorieCounter {
 		System.out.println("You should be drinking this much more/less ounces of water a day: " + waterNeeded);
 		
 		System.out.println("\n================================================================================================");
-		System.out.println("Hey " + newUser.getFullName() + "! Now list me what you ate today.");
+		System.out.println("            Hey " + newUser.getFullName() + "! Now list me what you ate today.");
 		System.out.println("================================================================================================");
 		
 		List<Macronutrients> macros = new ArrayList<>();
@@ -40,7 +40,7 @@ public class CalorieCounter {
 		
 		utils.Macromath(tmpMacros);
 		System.out.println("\n\n================================================================================================");
-		System.out.println("Would you like to see some healthy food suggestions? (Enter Y or y, any other charecter to exit.)");
+		System.out.println("Would you like to see some healthy food suggestions? (Enter Y or y, any other character to exit.)");
 		System.out.println("================================================================================================");
 		answer = scanner.next().charAt(0);
 		if(answer == 'Y' || answer == 'y') {
